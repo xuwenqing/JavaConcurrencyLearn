1b14e1072e44759b0c7ab1790795be8ab1e29543
xuwenqing <qing18363031040>
2016-05-19 22:31:55 +0800

    ch3待完成

---
+++ src/test/java/ch3/Test.java
@@ -0,0 +1,13 @@
+package ch3;
+
+/**
+ * Created by wenqing on 2016/5/19.
+ */
+public class Test {
+    //1.使用semaphore实现一个打印机的管理
+    //2.使用semaphore实现3个打印机的管理
+    //3.使用CountDownLatch实现视频会议系统
+    //3.1 会议线程，等待会议人数全部到达，CountDownLatch.await开始会议
+    //3.2 参与者线程，随机制定到达时间，然后CountDownLatch--
+    //4.在集合点的同步
+}
